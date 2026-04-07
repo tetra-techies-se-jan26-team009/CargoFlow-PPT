8f1d4f6bc8ae8dad993351de04acd491dabbfa83
Ripusudan Jha <ripusudankumarjha05@gmail.com>
2026-04-07 16:28:50 +0530

    updated chatbot

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
 from .database import Base, engine, SessionLocal
 from .models import User, UserRole
 from .routes import auth_routes, admin_routes, client_routes, agent_routes
+from .auth import hash_password
 
 from chatbot.app import router as chatbot_router
 import chatbot.app as chatbot_module
-from chatbot.rag_pipeline import SwiftShipRAG
-
-from .auth import hash_password
-from dotenv import load_dotenv
-import os
+from chatbot.rag_pipeline import CargoFlowRAG
 
 load_dotenv()
 
@@ -19,30 +19,34 @@ app.include_router(auth_routes.router)
 app.include_router(admin_routes.router)
 app.include_router(client_routes.router)
 app.include_router(agent_routes.router)
-app.include_router(chatbot_router)
+
+app.include_router(chatbot_router, prefix="/chatbot", tags=["Chatbot"])
 
 @app.on_event("startup")
 def startup():
     Base.metadata.create_all(bind=engine)
-
     db = SessionLocal()
-    admin = db.query(User).filter(User.role == UserRole.ADMIN).first()
 
-    if not admin:
-        new_admin = User(
-            name="Admin",
-            email=os.getenv("ADMIN_EMAIL"),
-            password_hash=hash_password(os.getenv("ADMIN_PASSWORD")),
-            phone=os.getenv("ADMIN_PHONE"),
-            role=UserRole.ADMIN,
-            is_active=True)
-        db.add(new_admin)
-        db.commit()
-    db.close()
+    try:
+        admin = db.query(User).filter(User.role == UserRole.ADMIN).first()
 
-    print("[main] Initializing Chatbot RAG...")
-    chatbot_module.rag = SwiftShipRAG()
-    print("[main] Chatbot Ready")
+        if not admin:
+            new_admin = User(
+                name="Admin",
+                email=os.getenv("ADMIN_EMAIL"),
+                password_hash=hash_password(os.getenv("ADMIN_PASSWORD")),
+                phone=os.getenv("ADMIN_PHONE"),
+                role=UserRole.ADMIN,
+                is_active=True)
+            db.add(new_admin)
+            db.commit()
+
+    finally:
+        db.close()
+
+    print("[main] Initializing CargoFlow chatbot...")
+    chatbot_module.rag = CargoFlowRAG()
+    print("[main] Chatbot ready.")
 
 @app.get("/health")
 def health():
