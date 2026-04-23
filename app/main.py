29379e0dad98d2df0ddfa368912a7a6f6432935c
Ripusudan Kumar Jha <ripusudankumarjha05@gmail.com>
2026-04-23 16:54:12 +0530

    Fix CORS origin URL format

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://cargo-flow-ppt.vercel.app/"
+        "https://cargo-flow-ppt.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
