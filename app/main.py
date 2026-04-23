b594bda5702c22385993faae4c9d1e56e8abe343
Ripusudan Jha <ripusudankumarjha05@gmail.com>
2026-04-23 16:39:43 +0530

    added frontend url in CORS

--- app/main.py
+++ app/main.py
@@ -20,7 +20,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=[
+        "http://localhost:5173",
+        "https://cargo-flow-ppt.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
